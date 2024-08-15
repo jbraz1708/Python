008b737dae3a9c64f3479890841da202a9491d74
João Victor Braz <89356306+jbraz1708@users.noreply.github.com>
2024-08-14 23:36:58 -0300

    Function + return

---
+++ funções/voto.py
@@ -0,0 +1,18 @@
+""" Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições."""
+
+
+def voto(ano): # variavel local
+    from datetime import date
+    atual = date.today().year 
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: Não vota!'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: Voto opcional!'
+    else:
+        return f'Com {idade} anos: Voto obrigatório!'
+         
+
+# programa principal
+nasc = int(input('Emq que ano você nasceu: '))
+print(voto(nasc))
